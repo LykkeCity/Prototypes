fbf8b907f973813d38c429a05a1208be8475c6b4
benjyz <benjamin.l.cordes@gmail.com>
2015-11-26 18:03:43 +0100

    show list of blocks

--- explorer/app.py
+++ explorer/app.py
@@ -18,26 +18,31 @@ def dateString(utime):
     dateFormat = '%Y-%m-%d'
     return datetime.datetime.fromtimestamp(int(utime)).strftime(dateFormat)
 
-
-def blocksinfo():
-    ''' blocks info '''
-
-    h = bitcoinLayer.getblockhash(1)
-    block = bitcoinLayer.getblock(h)
+def blockinfo():
+    """ info about one block """
     numTx = len(block.vtx)
+    h = bitcoinLayer.getblockhash(i)
+    block = bitcoinLayer.getblock(h)
+    infostr = '%i %s %s %s\n'%(i,dateString(block.nTime),block.difficulty, numTx)
+    return infostr
+
+def listblocksinfo():
+    ''' info about list of blocks '''
+
     blocks = []
     for i in range(1,1440,144):
-        blocks.append('%i %s %s %s\n'%(i,dateString(block.nTime),block.difficulty, numTx))
+        blocks.append(blockinfo(i))
     return blocks
 
 
 @app.route('/')
 def getblockinfo():
-    blocks = blocksinfo()
-    #print blocks
+    """ show some information about a list of blocks """
+    blocks = listblocksinfo()
     return render_template('blocks.html', blocks=blocks)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    port = 80
+    app.run(host='0.0.0.0',port=80)
     #blocks = blocksinfo(1)
     #print blocks
